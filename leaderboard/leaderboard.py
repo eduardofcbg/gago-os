f3ee27baa7db2118e2bde1ef9f6656f6cca6cd3a
BuildTools <eduardofcbg@gmail.com>
2020-11-23 00:17:46 +0000

    indentation

--- leaderboard/leaderboard.py
+++ leaderboard/leaderboard.py
@@ -4,26 +4,26 @@ from operator import itemgetter
 from dashing import VSplit, HGauge
 
 def create_ui(scores):
-	sorted_scores = sorted(scores.items(), key=itemgetter(1), reverse=True)
+  sorted_scores = sorted(scores.items(), key=itemgetter(1), reverse=True)
 
-	gauges = (
-		HGauge(val=score, title=name, border_color=5)
-		for name, score in sorted_scores
-	)
+  gauges = (
+    HGauge(val=score, title=name, border_color=5)
+    for name, score in sorted_scores
+  )
 
-	return VSplit(*gauges)
+  return VSplit(*gauges)
 
 
 if __name__ == '__main__':
-	while True:
-		sleep(1)
-		# scores = score_users()
+  while True:
+    sleep(1)
+    # scores = score_users()
 
-		import random
-		scores = {
-			'eduardo': random.choice([20, 30, 10, 50, 32]),
-			'lixo': random.choice([20, 30, 10, 50, 32]),
-		}
+    import random
+    scores = {
+      'eduardo': random.choice([20, 30, 10, 50, 32]),
+      'lixo': random.choice([20, 30, 10, 50, 32]),
+    }
 
-		ui = create_ui(scores)
-		ui.display()
+    ui = create_ui(scores)
+    ui.display()
