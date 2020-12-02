8235e16f616f8cfb603e0c322da59d7b1353158a
BuildTools <eduardofcbg@gmail.com>
2020-12-02 04:29:49 +0000

    formatting

--- leaderboard/leaderboard.py
+++ leaderboard/leaderboard.py
@@ -6,36 +6,34 @@ from dashing import VSplit, HSplit, HGauge
 from users import get_users
 from score import get_score
 
+
 def chunks(l, n):
-  for i in range(n):
-      yield l[i::n]
+    for i in range(n):
+        yield l[i::n]
+
 
 def create_ui(scores):
-  sorted_scores = sorted(scores.items(), key=itemgetter(1), reverse=True)
+    sorted_scores = sorted(scores.items(), key=itemgetter(1), reverse=True)
 
-  gauges = [
-    HGauge(val=score, title=name, border_color=5)
-    for name, score in sorted_scores
-  ]
+    gauges = [
+        HGauge(val=score, title=name, border_color=5) for name, score in sorted_scores
+    ]
 
-  max_height = 20
-  number_columns = int(len(gauges) / max_height) + 1
+    max_height = 20
+    number_columns = int(len(gauges) / max_height) + 1
 
-  columns = (
-    VSplit(*chunk)
-    for chunk in chunks(gauges, number_columns)
-  )
+    columns = (VSplit(*chunk) for chunk in chunks(gauges, number_columns))
 
-  return HSplit(*columns)
+    return HSplit(*columns)
 
 
-if __name__ == '__main__':    
-  while True:
-    sleep(1)
-    
-    scores = {}
-    for user in get_users():
-      scores[user] = get_score(user)
+if __name__ == "__main__":
+    while True:
+        sleep(1)
 
-    ui = create_ui(scores)
-    ui.display()
+        scores = {}
+        for user in get_users():
+            scores[user] = get_score(user)
+
+        ui = create_ui(scores)
+        ui.display()
--- leaderboard/users.py
+++ leaderboard/users.py
@@ -1,4 +1,3 @@
-
-def get_users():	
-	with open('/config/users.txt') as users_file:
-	    return users_file.read().splitlines()
+def get_users():
+    with open("/config/users.txt") as users_file:
+        return users_file.read().splitlines()
