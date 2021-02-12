60028d90af1c960b2da6af41383dd40ba2dcad24
BuildTools <eduardofcbg@gmail.com>
2021-02-12 17:16:51 +0000

    head

--- leaderboard/score/exercises/http_server/completed_log.sh
+++ leaderboard/score/exercises/http_server/completed_log.sh
@@ -8,20 +8,20 @@ which nginx
 
 systemctl is-active nginx --quiet && echo "nginx started"
 
-cat /var/www/html/index.nginx-debian.html | grep -i amigos
+cat /var/www/html/index.nginx-debian.html | grep -i amigos | head -1
 
 ls /var/www/html/bem-vindos.html
 
-cat /var/www/html/bem-vindos.html | grep -i site
+cat /var/www/html/bem-vindos.html | grep -i site | head -1
 
 ls /var/www/html/eu.html
 
-cat /var/www/html/eu.html | grep -i sou
+cat /var/www/html/eu.html | grep -i sou | head -1
 
-cat /var/www/html/index.nginx-debian.html | grep -E 'href="\/?bem-vindos.html"' | grep -i "Boas-vindas"
+cat /var/www/html/index.nginx-debian.html | grep -E 'href="\/?bem-vindos.html"' | grep -i "Boas-vindas" | head -1
 
-cat /var/www/html/index.nginx-debian.html | grep -E 'href="\/?eu.html"' | grep -i "Mais sobre mim"
+cat /var/www/html/index.nginx-debian.html | grep -E 'href="\/?eu.html"' | grep -i "Mais sobre mim" | head -1
 
-cat /var/www/html/bem-vindos.html | grep -E 'href="\/"'
+cat /var/www/html/bem-vindos.html | grep -E 'href="\/"' | head -1
 
-cat /var/www/html/eu.html | grep -E 'href="\/"'
+cat /var/www/html/eu.html | grep -E 'href="\/"' | head -1
