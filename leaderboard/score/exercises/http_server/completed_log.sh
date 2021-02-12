40b43a0995abb3bdfc63b648093f521dd397fc54
BuildTools <eduardofcbg@gmail.com>
2021-02-12 18:21:30 +0000

    relative and absolute

--- leaderboard/score/exercises/http_server/completed_log.sh
+++ leaderboard/score/exercises/http_server/completed_log.sh
@@ -18,9 +18,9 @@ ls /var/www/html/eu.html
 
 cat /var/www/html/eu.html | grep -i sou | head -1
 
-cat /var/www/html/index.nginx-debian.html | grep -P 'href="\/?bem-vindos.html"' | grep -i "Boas-vindas" | head -1
+cat /var/www/html/index.nginx-debian.html | grep -P 'href=".*\/?bem-vindos.html"' | grep -i "Boas-vindas" | head -1
 
-cat /var/www/html/index.nginx-debian.html | grep -P 'href="\/?eu.html"' | grep -i "Mais sobre mim" | head -1
+cat /var/www/html/index.nginx-debian.html | grep -P 'href=".*\/?eu.html"' | grep -i "Mais sobre mim" | head -1
 
 match_root='(href="\/")|(href="http:\/\/\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}\/?")|(href="\/*index.nginx-debian.html")'
 
