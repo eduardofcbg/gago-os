1e4e2111917bc41ff420ff3bc38cc9f612e4235d
Eduardo <eduardofcbg@gmail.com>
2021-03-16 15:35:38 +0000

    chown

--- src/score/exercises/scripting2/score.py
+++ src/score/exercises/scripting2/score.py
@@ -1,8 +1,8 @@
 from score.execute.local import run_command
 
 
-def run_score_script(work_dir):
-    command = f"/src/score/exercises/scripting2/score.sh {work_dir}"
+def run_score_script(user, work_dir):
+    command = f"/src/score/exercises/scripting2/score.sh {user} {work_dir}"
 
     return int(run_command(command, cwd=work_dir))
 
@@ -13,4 +13,4 @@ def get_max_score():
 def score(user):
     work_dir = f"/home/{user}"
 
-    return int(run_score_script(work_dir) / get_max_score() * 100)
+    return int(run_score_script(user, work_dir) / get_max_score() * 100)
