4fb485808275e4bf710581d885fe4da928b691e9
Eduardo Goncalves <eduardofcbg@gmail.com>
2021-03-16 05:31:24 +0000

    scripting2 exercise scoring

--- src/score/exercises/scripting2/score.py
+++ src/score/exercises/scripting2/score.py
@@ -1,19 +1,17 @@
 from score.execute.local import run_command
 
 
-def run_script(work_dir):
-    command = f"{work_dir}/scripting2"
+def run_score_script(work_dir):
+    command = f"./score.sh {work_dir}"
 
-    return run_command(command)
+    return int(run_command(command))
 
-script_out_goal = 
+def get_max_score():
+    return 17 
 
 
 def score(user):
     work_dir = f"/home/{user}"
-    script_out = run_script(work_dir)
 
-    if error_count > max_number_errors:
-        raise ValueError(f"Found {work_dir} errors, than expected {max_number_errors}")
+    return int(run_score_script(work_dir) / get_max_score() * 100)
 
-    return int(100 * (1 - error_count / max_number_errors))
--- src/score/exercises/scripting2/score.sh
+++ src/score/exercises/scripting2/score.sh
@@ -3,11 +3,12 @@
 workdir=$1
 solveddir="/src/score/exercises/scripting2/solved"
 
-./${workdir}/scripting1 | cmp - ${solveddir}/out | awk '{print $NF-1}'
+score=$(cmp <(${workdir}/scripting1) <(${solveddir}/scripting1) && bash ${solveddir}/scripting1 | wc -l || awk '{print $NF-1}')
 
-cmp -s ${workdir}/jogos_nintendo ${solveddir}/jogos_nintendo && echo 1
+cmp -s ${workdir}/jogos_nintendo ${solveddir}/jogos_nintendo && score=$((score+1))
 
-cmp -s ${workdir}/jogos_open_world ${solveddir}/jogos_open_world && echo 1
+cmp -s ${workdir}/jogos_open_world ${solveddir}/jogos_open_world && score=$((score+1))
 
-cmp -s ${workdir}/jogos_shadow ${solveddir}/jogos_shadow && echo 1
+cmp -s ${workdir}/jogos_shadow ${solveddir}/jogos_shadow && score=$((score+1)) 
 
+echo $score
