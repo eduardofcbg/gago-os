c8b0f99f4e6b78cc4e11e6cf29ac52879c15d2ea
Eduardo Goncalves <eduardofcbg@gmail.com>
2021-03-24 16:57:41 +0000

    do not handle IOError

--- src/score/execute/local.py
+++ src/score/execute/local.py
@@ -2,13 +2,10 @@ from subprocess import Popen, PIPE
 
 
 def run_command(command, cwd=None):
-    try:
-        process = Popen(
-            command, stdout=PIPE, stderr=PIPE, shell=True, executable="/bin/bash", cwd=cwd
-        )
-        stdout, stderr = process.communicate()
-    except OSError as e:
-        raise IOError(e)
+    process = Popen(
+        command, stdout=PIPE, stderr=PIPE, shell=True, executable="/bin/bash", cwd=cwd
+    )
+    stdout, stderr = process.communicate()
 
     if stderr:
         raise RuntimeError(stderr)
